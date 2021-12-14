fcb1ab374646318c257725f2410ca1aaa7443757
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-12-14 18:26:02 -0500

    Add more tests

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -6,10 +6,20 @@ RSpec.describe 'movies show page' do
 
     @ark = @universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
 
+    @harrison = Actor.create!(name: 'Harrison Ford', age: 79)
+    @karen = Actor.create!(name: 'Karen Allen', age: 70)
+
+    @ark.actors << @harrison
+    @ark.actors << @karen
     visit "/movies/#{@ark.id}"
   end
 
   it 'shows the title, creation year, genre of the movie' do
-    expect(page).to have_content(@ark.name)
+    save_and_open_page
+    expect(page).to have_content(@ark.title)
+    expect(page).to have_content(@ark.creation_year)
+    expect(page).to have_content(@ark.genre)
+    expect(page).to have_content(74.5)
+
   end
 end
