0f40999082c01d87020d6d83183231cfbf3258a4
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-12-14 18:25:28 -0500

    Add more data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
-universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+@universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
 
-universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
-universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+@ark = @universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+@shrek = @universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+
+
+@harrison = Actor.create!(name: 'Harrison Ford', age: 79)
+@karen = Actor.create!(name: 'Karen Allen', age: 70)
+
+@ark.actors << @harrison
+@ark.actors << @karen
